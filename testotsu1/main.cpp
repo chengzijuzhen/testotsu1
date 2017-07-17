e9891cfc3f2b9752282aadd7bcab17a0d801d45e
kuangyan <2992811525@qq.com>
2017-07-17 10:18:59 +0800

    完成了去除颅骨及颅骨以外区域的功能，可以分割出脑脊液；完成了去除脑脊液及脑脊液以外部分，可以分割出脑实质，但有瑕疵。

--- testotsu1/main.cpp
+++ testotsu1/main.cpp
@@ -8,7 +8,8 @@ using namespace std;
 
 int OSTU_Alg_Threshold(const Mat image,int value1,int value2);//对图片image中灰度值在value1-value2之间的像素点进行求阈值运算
 int Imagine_Convert(const Mat image,int value1,int value2);//对图片image中灰度值在value1-value2之间的像素点灰度置为255(白)，其他像素点均置0(黑)
-Mat deleteBone(const Mat image,int threshold);//去出颅骨
+Mat deleteBone(const Mat image,int threshold);//去除颅骨区域及以外部分
+Mat deleteNJY(const Mat image,const Mat markImage,int threshold);//去除脑脊液区域及以外部分
 
 int main()
 {
@@ -19,42 +20,39 @@ int main()
 		return -1;
 	}
 	imshow("原图",image);
-
-	Mat image_LUGU;  //定义颅骨
-	Mat image_NSZ;   //定义脑实质
-	Mat image_NJY;   //定义脊液
-
-	cvtColor(image,image_LUGU,CV_RGB2GRAY);  //转换为单通道灰度图,初始化image_LUGU
-	cvtColor(image,image_NSZ,CV_RGB2GRAY);  //转换为单通道灰度图,image_NSZ
-	cvtColor(image,image_NJY,CV_RGB2GRAY);  //转换为单通道灰度图,image_NJY
 	cvtColor(image,image,CV_RGB2GRAY);  //转换为单通道灰度图
+
+	Mat copy;
+	Mat copyNJY;
 
 	int thresholdValue1=OSTU_Alg_Threshold(image,0,255);  //分离出前景(脑实质+颅骨)与背景(脑脊液+图片背景)
 	cout<<"阈值1(分离出前景(脑实质+颅骨)与背景(脑脊液+图片背景))为："<<thresholdValue1<<endl;
 
 	int thresholdValue2=OSTU_Alg_Threshold(image,thresholdValue1,255);  //初次分离出颅骨
-	cout<<"阈值2为："<<thresholdValue2<<endl;
-
 	int thresholdValue3=OSTU_Alg_Threshold(image,thresholdValue2,255);  //对分离出颅骨进行一次迭代优化
-	cout<<"阈值3为："<<thresholdValue3<<endl;
 
 	int thresholdValue4=(thresholdValue2+thresholdValue3)/2;  //对两次分离颅骨的阈值取平均值
-	cout<<"阈值4(分离颅骨)为："<<thresholdValue4<<endl;
+	cout<<"阈值4(分割颅骨)为："<<thresholdValue4<<endl;
 		
 	int thresholdValue5=OSTU_Alg_Threshold(image,0,thresholdValue1); //分离出脑脊液
-	cout<<"阈值5为："<<thresholdValue5<<endl;
+	cout<<"阈值5为(分割脑脊液)："<<thresholdValue5<<endl;
 
-	Mat image1=deleteBone(image,thresholdValue2);//去出颅骨，此时选用的是初次分离颅骨的阈值
-	imshow("去除颅骨",image1);
+	Mat img_deleteBone=deleteBone(image,thresholdValue2);//去出颅骨及以外部分，此时选用的是初次分离颅骨的阈值
+	imshow("去除颅骨",img_deleteBone);
+
+	image.copyTo(copy);
+	img_deleteBone.copyTo(copyNJY);
+
+	Imagine_Convert(img_deleteBone,0,thresholdValue5);//将脑脊液部分的像素点灰度置255(白)	
+	imshow("脑脊液",img_deleteBone);
+
+	imshow("图片备份",copy);
+	Imagine_Convert(copyNJY,0,27);//将脑脊液部分的像素点灰度置255(白)
+	Mat img_deleteNJY=deleteNJY(copy,copyNJY,255);//去出脑脊液及以外部分，此时选用的是初次分离颅骨的阈值
+	imshow("去除脑脊液",img_deleteNJY);
 	
-	Imagine_Convert(image1,0,thresholdValue5);//将脑脊液部分的像素点灰度置255(白)
-	imshow("脑脊液",image1);
-		
-	Imagine_Convert(image_LUGU,thresholdValue4,255);//将颅骨部分的像素点灰度置255(白)
-	imshow("颅骨",image_LUGU);	
-
-	Imagine_Convert(image_NSZ,65,90);//将脑实质部分的像素点灰度置255(白)
-	imshow("脑实质",image_NSZ);
+	Imagine_Convert(img_deleteNJY,65,90);//将脑实质部分的像素点灰度置255(白)
+	imshow("脑实质",img_deleteNJY);
 
 	waitKey();
 	system("pause");
@@ -130,36 +128,36 @@ int Imagine_Convert(const Mat image,int value1,int value2)
 		{ 
 			data[i]=255;
 		}
-	}
+	}	
 	return 0;
 }
 
+//去除颅骨区域及以外部分
 Mat deleteBone(const Mat image,int threshold)
 {
 	uchar*data=image.data;
-	int boneFlag[256][256]={0};
+	int flag[256][256]={0};
 
-	//对图像中颅骨部分的灰度值置0，即去除颅骨，将其变成背景
+	//对图像中目标区域的灰度值置0，即去除目标区域，将其变成背景
 	for(int i=0; i<image.rows; i++)
 	{
 		for(int j=0; j<image.cols; j++)
 		{
-			if(data[i*image.step+j]>= threshold) //根据阈值，该灰度值属于颅骨区域
+			if(data[i*image.step+j]>= threshold) //灰度值大于等于阈值，属于目标区域
 			{
 				data[i*image.step+j] = 0;
-				boneFlag[i][j] = 1;//颅骨标记
-
+				flag[i][j] = 1;//标记成目标区域
 			}
-		}		
+		}			
 	}
-
+	
 	//再次遍历图像，找到每一行中第一个属于颅骨的像素点，将该点前面的所有点置为0。
 	//即将图像的左半部分颅骨及颅骨以外区域置为0
 	for(int n=0;n<image.rows;n++)
 	{
 		for(int m=0;m<image.cols;m++)
 		{
-			if(data[n*image.step+m]==0 && boneFlag[n][m]==1) 
+			if(data[n*image.step+m]==0 && flag[n][m]==1) 
 			{
 				for(int k=0;k<m;k++)
 					data[n*image.step+k]=0;
@@ -174,7 +172,7 @@ Mat deleteBone(const Mat image,int threshold)
 	{
 		for(int m=image.cols;m>=0;m--)
 		{
-			if(data[n*image.step+m]==0 && boneFlag[n][m]==1) 
+			if(data[n*image.step+m]==0 && flag[n][m]==1) 
 			{
 				for(int k=image.cols;k>m;k--)
 					data[n*image.step+k]=0;
@@ -188,7 +186,7 @@ Mat deleteBone(const Mat image,int threshold)
 	{
 		for(int m=0;m<image.cols;m++)
 		{
-			if(boneFlag[n][m]!=1) 
+			if(flag[n][m]!=1) 
 				data[n*image.step+m]=0;
 			else
 			{
@@ -196,13 +194,90 @@ Mat deleteBone(const Mat image,int threshold)
 				{
 					for(int m=0;m<image.cols;m++)
 					{
-						if(boneFlag[n][m]!=1) 
+						if(flag[n][m]!=1) 
 							data[n*image.step+m]=0;	
 						else
-						  return image;
+							return image;
 					}
 				}			
 			}				
 		}	
-	}	
+	}
+	return image;
 }
+
+//去除脑脊液区域及以外部分
+Mat deleteNJY(const Mat image,const Mat markImage,int threshold)
+{
+	uchar*markImageData=markImage.data;//提取脑脊液后的图像，用来标记脑脊液区域
+	uchar*data=image.data;//待处理图片
+	int flag[256][256]={0};
+
+	//对图像中目标区域的灰度值置0，即去除目标区域，将其变成背景
+	for(int i=0; i<image.rows; i++)
+	{
+		for(int j=0; j<image.cols; j++)
+		{
+			if(markImageData[i*markImage.step+j]>= threshold) //灰度值大于等于阈值，属于目标区域
+			{
+				data[i*image.step+j] = 0;
+				flag[i][j] = 1;//标记成目标区域
+			}
+		}			
+	}
+
+	//再次遍历图像，找到每一行中第一个属于颅骨的像素点，将该点前面的所有点置为0。
+	//即将图像的左半部分颅骨及颅骨以外区域置为0
+	for(int n=0;n<image.rows;n++)
+	{
+		for(int m=0;m<image.cols;m++)
+		{
+			if( flag[n][m]==1 && data[n*image.step+m]==0) 
+			{
+				for(int k=0;k<m;k++)
+					data[n*image.step+k]=0;
+				break;//将第一个属于颅骨的像素点前面的所有点置为0后，跳转下一行。
+			}			
+		}	
+	}
+	
+	//同理，再次遍历图像，找到每一行中最后一个属于颅骨的像素点，将该点后面的所有点置为0。
+	//即将图像的右半部分颅骨及颅骨以外区域置为0
+	for(int n=0;n<image.rows;n++)
+	{
+		for(int m=image.cols;m>=0;m--)
+		{
+			if(flag[n][m]==1 && data[n*image.step+m]==0) 
+			{
+				for(int k=image.cols;k>m;k--)
+					data[n*image.step+k]=0;
+				break;//将最后一个属于颅骨的像素点后面的所有点置为0后，跳转下一行。
+			}			
+		}	
+	}
+	
+	//去除头皮及图像上下半部分的非0背景。
+	for(int n=0;n<image.rows;n++)
+	{
+		for(int m=0;m<image.cols;m++)
+		{
+			if(flag[n][m]!=1) 
+				data[n*image.step+m]=0;
+			else
+			{
+				for(int n=image.rows-1;n>=0;n--)
+				{
+					for(int m=0;m<image.cols;m++)
+					{
+						if(flag[n][m]!=1) 
+							data[n*image.step+m]=0;	
+						else
+							return image;
+					}
+				}			
+			}				
+		}	
+	}
+	return image;
+}
+
